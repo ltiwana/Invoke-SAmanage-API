728f67f93e4c603d21c9697e50d7fa56b3251354
LT (Loveparteek Tiwana) <35739930+ltiwana@users.noreply.github.com>
2019-03-22 23:50:09 -0400

    Create Invoke-SAmanageJsonAPI.psm1

---
+++ Invoke-SAmanageJsonAPI.psm1
@@ -0,0 +1,95 @@
+Function Invoke-SAmanageJsonAPI {
+    
+    Param (        
+        [int]$Results = 100,
+        [Switch]$AllPages = $False,
+        [string]$URI,        
+        [string]$BearerToken,
+        [string]$Method,
+        $Body
+    )
+
+    [int]$i = 1
+    [array]$APIdataJson = @()
+    
+
+    Write-Verbose "Info: Getting things ready for the API call"
+    Write-Verbose "Info: Setting up JSON API header"
+    $Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $Headers.Add("Accept", 'application/vnd.samanage.v2.1+json')
+    $Headers.add("Content-Type", 'application/json')
+    $Headers.Add("X-Samanage-Authorization", "Bearer $BearerToken")
+
+    #$Headers.Add("Accept", 'application/vnd.samanage.v2.1+xml')
+    #$Headers.Add("X-Samanage-Authorization", 'Bearer bG92ZXBhcnRlZWtAbHVzaC5jb20=:eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjo0NTk2Njk4LCJnZW5lcmF0ZWRfYXQiOiIyMDE5LTAyLTExIDE2OjAyOjUwIn0.B6i7-do3IQxbmfcSNVyREA_paZdRRaYdD_1nigj-osCUdLgcbB81SVp8N0Z4WucaGu_QFBDFlBuFaSIVWv8c4Q')
+    
+    Write-Verbose "Making $Method API call"
+
+    Switch ($Method) {
+
+        Get {
+            
+
+
+            Write-Verbose "Action: Making first Get API call"
+
+            $APIdata = Invoke-WebRequest -URI $URI -Method $Method -Headers $Headers -Verbose
+            $APIdataJson += ConvertFrom-Json $APIdata
+
+            $APITotalPages = $APIdata.Headers.'X-Total-Pages'
+
+            Write-Verbose "Info: Checking API header for total number content pages"
+            Write-Verbose "Info: $APITotalPages pages found"
+
+            if ($APITotalPages -gt "1" -and $AllPages -eq $True) {
+        
+                Write-Verbose "Info: User requested all pages"
+                Write-Verbose "Info: Getting results from each page"
+
+                do {
+                    
+                    $i++
+
+                    if ($VerbosePreference -eq "Continue") {
+                       
+                        $Percentage = [math]::Round($i/$APITotalPages*100)
+                        Write-Progress -Activity ("Completed $i out of $APITotalPages") -Status "Current progress: $Percentage%"  -PercentComplete $Percentage
+                    } 
+            
+                    
+                    Write-Verbose "Info: Currently at page $i out $APITotalPages"
+                    $NewURI = ($URI + "?page=$i")
+                    Write-Verbose "Action: Making the the API call on $NewURI"
+                    $APIdataJson += ConvertFrom-Json (Invoke-WebRequest -URI $NewURI -Method $Method -Headers $Headers -Verbose)
+
+                } while ($i -ne $APITotalPages)
+
+
+            }
+    
+            Write-Verbose "Info: API call completed"
+            Write-Verbose "Info: Returning all the API data"
+            return $APIdataJson
+
+        }
+
+        Put {
+            
+            if ($Body -eq $null -or !$body) {
+                Write-Warning "Body is required to make Put API call"
+                Write-Error "Body parameter was not used"
+            }
+            else {
+                $APIdataJson += ConvertFrom-Json (Invoke-WebRequest -URI $URI -Headers $Headers -Method $Method -Body $BodyNull -Verbose)
+                return $APIdataJson
+            }
+
+        }
+
+
+
+    }
+
+
+    
+}
